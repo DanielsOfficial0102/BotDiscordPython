1c9119b4728e3bacb743067b116784ccd7ec1d49
Daniel Lopes <69876467+DanielsOfficial0102@users.noreply.github.com>
2021-09-14 22:58:17 -0300

    main e assets

---
+++ main.py
@@ -0,0 +1,22 @@
+#importações nessesarias
+
+import discord
+from discord.ext import commands
+import random
+
+#Comando que irá ativar o Bot no discord
+client = commands.Bot(command_prefix = ",", case_insensitive = True)
+
+@client.event
+async def on_ready():
+  print('Entramos como {0.user}' .format(client))
+
+#Aqui é a ação que sera executada pelo usuario e respondida pelo bot em ctx.send
+@client.command()
+async def ola(ctx):
+  await ctx.send(f'Olá, {ctx.author}, Como vai ?')
+
+#insira seu id do bot em Cliente.run
+client.run('')
+
+#caso seja da sua vontade, usufrua esse codigo, acrescente mais camadas, funções...
